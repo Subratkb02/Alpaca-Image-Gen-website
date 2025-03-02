1e603e8473f74fac8fd7b91325d5c244f46dfacf
Subratkb02 <worksubrat2002@gmail.com>
2025-03-02 22:48:26 +0530

    Updated Files

---
+++ src/data.jsx
@@ -0,0 +1,48 @@
+export const alpacaObjects = {
+    accessories: ["earings", "flower", "glasses", "headphone"],
+    ears: ["default", "tilt-backward", "tilt-forward"],
+    eyes: ["default", "angry", "naughty", "panda", "smart", "star"],
+    hair: ["default", "bang", "curls", "elegant", "fancy", "quiff", "short"],
+    leg: [
+      "default",
+      "bubble-tea",
+      "cookie",
+      "game-console",
+      "tilt-backward",
+      "tilt-forward",
+    ],
+    mouth: ["default", "astonished", "eating", "laugh", "tongue"],
+    neck: ["default", "bend-backward", "thick"],
+    backgrounds: [
+      "blue50",
+      "blue60",
+      "blue70",
+      "darkblue30",
+      "darkblue50",
+      "darkblue70",
+      "green50",
+      "green60",
+      "green70",
+      "grey40",
+      "grey70",
+      "grey80",
+      "red50",
+      "red60",
+      "red70",
+      "yellow50",
+      "yellow60",
+      "yellow70",
+    ],
+  };
+  
+  export const alapacaKey = Object.keys(alpacaObjects);
+  export const AlapacaType = {
+    accessories: "",
+    ears: "",
+    eyes: "",
+    hair: "",
+    leg: "",
+    mouth: "",
+    neck: "",
+    backgrounds: "",
+  };
